eb93ef8a14854f48b52fdede883986c473b2b39f
Rahul Dogra <rahuldogra1998@gmail.com>
2020-12-22 02:33:59 +0530

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,97 @@
 import { ReactElement } from 'react';
+import { Typography } from 'antd';
 import Header from './Header';
 import Footer from './Footer';
 import Staff from './Staff';
+import Card from './Card';
 import './App.scss';
+
+const { Title } = Typography;
 
 const App = (): ReactElement => (
   <div className="app">
     <Header />
-    <div className="div__body"><Staff /></div>
+    <div className="div__body">
+      <Title className="featured__section" level={4}>Featured Live Sessions</Title>
+      <div className="div__cards">
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-marketing.4dc37fcd.png"
+            title="Marketing with Zero Budget"
+            staff="Alexa Smith"
+            time="Going live in 2hours, 25mins"
+            price="$23.99"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-6.1fc87047.png"
+            title="From Intern to Director of Marketing at TikTok in Less than 2 Years"
+            staff="Michaela Goldberg"
+            time="Going live in 5 hours, 8 minutes"
+            price="$59.99"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-7.d07c06e2.png"
+            title="Running an Online Business Overseas"
+            staff="James Perry"
+            time="Going live in 1 month, 23 days"
+            price="$25.00"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-8.812d0904.png"
+            title="Using A Couple Robinhood Option Trades to Fund your Venture"
+            staff="Derrick Michaels"
+            time="Going live in 3 days, 2 hours"
+            price="$35.99"
+          />
+        </div>
+      </div>
+      <Title className="featured__section" level={4}>Today Live Sessions</Title>
+      <div className="div__cards">
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-1.f557f0b1.png"
+            title="What it takes to Build a Billion Dollar Mobile App"
+            staff="Kevin Systrom"
+            time="Going live in 2 hours, 10 mins"
+            price="$17.00"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-2.de536f30.png"
+            title="Starting an e-Commerce Business during COVID"
+            staff="John Barry"
+            time="Going live in 6 hours, 24 mins"
+            price="$17.00"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-3.e7803b51.png"
+            title="Growth Hacking Facebook Ads to $50M in Sales"
+            staff="Jessica Hollins"
+            time="Going live in 4 hours, 2 minutes"
+            price="$32.00"
+          />
+        </div>
+        <div className="card">
+          <Card
+            image="https://beta.gura.io/static/media/Thumbnail-4.52dbfe6d.png"
+            title="Launching a Shopify Site in Less than 10 mins"
+            staff="Michella Jackson"
+            time="Going live in 3 hours, 20 minutes"
+            price="$48.00"
+          />
+        </div>
+      </div>
+      <Staff />
+    </div>
     <Footer />
   </div>
 );
---
+++ src/Card.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Typography, Image } from 'antd';
+import 'antd/dist/antd.css';
+import './Card.scss';
+
+const { Title, Paragraph } = Typography;
+
+type CardProps = {
+  image: string,
+  title: string,
+  staff: string,
+  time: string,
+  price: string,
+};
+
+const Card: React.FC<CardProps> = ({
+  // eslint-disable-next-line react/prop-types
+  image, title, staff, time, price,
+}) => (
+  <div className="card__container">
+    <div className="card__row">
+      <Image src={image} alt="staff_image" />
+      <div className="card__detail">
+        <Title className="bold-text" level={4}>{title}</Title>
+        <Paragraph className="instructor">{staff}</Paragraph>
+        <Paragraph className="time__left grey-text">{time}</Paragraph>
+        <Paragraph className="price__tag bold-text">{price}</Paragraph>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
